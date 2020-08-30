1a3d57b54b82630977a042b84bc054ab1c846242
ashleigh-lyman <ajlyman@my365.bellevue.edu>
2020-08-30 11:55:31 -0500

    added new folder

---
+++ week-8/in-n-out-books/src/app/wishlist/wishlist.component.ts
@@ -0,0 +1,32 @@
+/**
+ * Title: wishlist.component.ts
+ * Author: Ashleigh Lyman
+ * Date: 13 August 2020
+ * Description: wishlist component page
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+import { IWishlistItem } from '../wishlist-item.interface';
+
+@Component({
+  selector: 'app-wishlist',
+  templateUrl: './wishlist.component.html',
+  styleUrls: ['./wishlist.component.css']
+})
+export class WishlistComponent implements OnInit {
+
+  items: Array<IWishlistItem> = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateItemsHandler(item: IWishlistItem) {
+    console.log('Inside the wishlist.component.ts (parent) - updateItemsHandler() function');
+    console.log(item);
+    this.items.push(item);
+    console.log(this.items);
+  }
+}
